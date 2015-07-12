614d685e0c803fad16d85349c043479a74b655d6
root <root@raspberrypi.(none)>
2015-07-12 22:40:51 +0200

    Added support for reporting to Librato

---
+++ README.md
@@ -0,0 +1,40 @@
+domoticz-cli
+============
+
+This simple tool is a CLI for [Domoticz](http://www.domoticz.com/).
+
+You can power on/off light switches, list sensor values and much more. Domoticz-cli can also produce Graphite compatible output or report directly to [Librato](http://www.librato.com|Librato).
+
+```usage: domoticz-cli.py [-h] [--host HOST] [--port PORT] [--debug]
+                       [--list-switches] [--list-sensors]
+                       [--list-sensors-graphite] [--list-sensors-librato]
+                       [--report-prefix NAME] [--list-scenes] [--list-groups]
+                       [--toggle-switch NAME] [--run-scene NAME]
+                       [--get-sunrise] [--librato-user LIBRATO_USER]
+                       [--librato-token LIBRATO_TOKEN]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --host HOST           Host to connect to
+  --port PORT           Port to connect to
+  --debug               Display more debug information
+  --list-switches       List all switches and their state
+  --list-sensors        List all sensors and their value
+  --list-sensors-graphite
+                        List all sensors in Graphite readable format
+  --list-sensors-librato
+                        Report all sensors to Librato
+  --report-prefix NAME  If set, use this string as prefix for all Graphite and
+                        Librato data
+  --list-scenes         List all scenes
+  --list-groups         List all groups
+  --toggle-switch NAME  Toggle a switch
+  --run-scene NAME      Run a scene
+  --get-sunrise         Get and display time for sunrise and sunset
+  --librato-user LIBRATO_USER
+                        Librato user name. If also Librato token is set,
+                        report sensors to Librato.
+  --librato-token LIBRATO_TOKEN
+                        Librato token. If also Librato user is set, report
+                        sensors to Librato.
+```
--- domoticz-cli.py
+++ domoticz-cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# magnus@appelquist.name
 
 import requests, json, argparse, sys, time, datetime
 
@@ -14,12 +15,16 @@ parser.add_argument("--debug", action="store_true", help="Display more debug inf
 parser.add_argument("--list-switches", action="store_true", help="List all switches and their state")
 parser.add_argument("--list-sensors", action="store_true", help="List all sensors and their value")
 parser.add_argument("--list-sensors-graphite", action="store_true", help="List all sensors in Graphite readable format")
-parser.add_argument("--graphite-prefix", metavar='NAME', required=False, help="If set, use this string as prefix for all Graphite data")
+parser.add_argument("--list-sensors-librato", action="store_true", help="Report all sensors to Librato")
+parser.add_argument("--report-prefix", metavar='NAME', required=False, help="If set, use this string as prefix for all Graphite and Librato data")
 parser.add_argument("--list-scenes", action="store_true", help="List all scenes")
 parser.add_argument("--list-groups", action="store_true", help="List all groups")
 parser.add_argument("--toggle-switch", action="store", help="Toggle a switch", metavar='NAME')
 parser.add_argument("--run-scene", action="store", help="Run a scene", metavar='NAME')
 parser.add_argument("--get-sunrise", action="store_true", help="Get and display time for sunrise and sunset")
+parser.add_argument("--librato-user", help="Librato user name. If also Librato token is set, report sensors to Librato.", required=False)
+parser.add_argument("--librato-token", help="Librato token. If also Librato user is set, report sensors to Librato.", required=False)
+
 args = parser.parse_args()
 
 debug = args.debug
@@ -54,8 +59,8 @@ if args.list_sensors_graphite:
 
         graphite_path = result["Name"].lower().replace(" ","-").encode("ascii","ignore")
         #graphite_path = result["Name"].lower().replace(" ","-")
-        if args.graphite_prefix:
-            graphite_path = args.graphite_prefix+"."+graphite_path
+        if args.report_prefix:
+            graphite_path = args.report_prefix+"."+graphite_path
         timestamp = int(time.mktime(datetime.datetime.strptime(result["LastUpdate"], "%Y-%m-%d %H:%M:%S").timetuple()))
 
         if result["Type"] == "Temp":
@@ -73,6 +78,52 @@ if args.list_sensors_graphite:
 
         if result["Type"] == "RFXMeter":
             print u"%s.counter %d %d" % (graphite_path, int(result["Data"].split(" ")[0].replace(".","")), timestamp)
+
+if args.list_sensors_librato and args.librato_user and args.librato_token:
+    import librato
+
+    librato_api = librato.connect(args.librato_user, args.librato_token)
+    q = librato_api.new_queue()
+    q.add('temperature', 23.1, source='dowstairs')
+    q.add('num_requests', 100, type='counter', source='server1')
+    q.add('num_requests', 102, type='counter', source='server2')
+
+    data = _get_request("type=devices&filter=all&used=true&order=Name")
+
+    nbr_values = 0
+    for result in data["result"]:
+
+        librato_path = result["Name"].lower().replace(" ","-").encode("ascii","ignore")
+        if args.report_prefix:
+            librato_path = args.report_prefix+"."+librato_path
+
+        if result["Type"] == "Temp":
+            q.add(u"%s.temperature" % librato_path, result["Temp"])
+            nbr_values += 1
+
+        if result["Type"] == "Temp + Humidity":
+            q.add(u"%s.temperature" % librato_path, result["Temp"])
+            q.add(u"%s.humidity" % librato_path, result["Temp"])
+            nbr_values += 2
+
+        if result["Type"] == "Usage":
+            q.add(u"%s.energy" % librato_path, float(result["Data"].split(" ")[0]))
+            nbr_values += 1
+
+        if result["Type"] == "Value":
+            q.add(u"%s.value" % librato_path, int(result["Data"]))
+            nbr_values += 1
+
+        if result["Type"] == "RFXMeter":
+            q.add(u"%s.counter" % librato_path, float(result["Data"].split(" ")[0].replace(".","")), type = "counter")
+            nbr_values += 1
+
+    print "Reported %d values to Librato." % nbr_values
+    q.submit()
+
+if args.list_sensors_librato and (not args.librato_user or not args.librato_token):
+    print "If reporting metrics to Librato you must also provide --librato-user and --librato-token"
+    sys.exit(1)
 
 if args.list_switches:
     data = _get_request("type=devices&filter=all&used=true&order=Name")
